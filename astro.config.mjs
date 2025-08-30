158dfac624bce00d6f5072ee0faf2315cb50b10c
6041579+Chrispassold <6041579+Chrispassold@users.noreply.github.com>
2025-08-30 10:55:36 -0300

    Refactor Astro configuration and update Tailwind CSS integration; replace @astrojs/tailwind with @tailwindcss/vite, adjust global styles, and remove unused postcss.config.mjs file for improved build process.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,8 +12,9 @@ export default defineConfig({
   
   integrations: [
     react(),
-    tailwind({
-      applyBaseStyles: false, // Usaremos nosso próprio global.css
-    })
   ],
+  
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
